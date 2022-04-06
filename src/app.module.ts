b88b52980e47c27350a9b4b07a4cca223c03fa79
Michael <chael58@gmail.com>
2022-04-06 15:04:43 -0300

    ajuste

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
+const url = process.env.MONGO_URL || 'localhost';
+
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(
       'mongodb+srv://bancoteste:teste1992@cluster0.yzedg.mongodb.net/test',
+      // 'mongodb://localhost:27017/db?authSource=admin&readPreference=primary',
+      // 'mongodb://mongodb:3000/DB',
+      // `mongodb://testing:12345678@docdb-2022-04-06-17-28-37.cblscjwbivbz.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false`,
     ),
   ],
   controllers: [],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { User, UserDocument } from './entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -20,20 +19,6 @@ export class UsersService {
 
   findOne(id: string) {
     return this.userModel.findById(id);
-  }
-
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return this.userModel.findByIdAndUpdate(
-      {
-        _id: id,
-      },
-      {
-        updateUserDto,
-      },
-      {
-        new: true,
-      },
-    );
   }
 
   remove(id: string) {
